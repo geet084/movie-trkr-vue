45a94d076e06f38f7ff35fa60e1d817a354c9ed3
Travis Gee <geet084@gmail.com>
2020-03-17 15:51:10 -0600

    Adds nav dropdown component

---
+++ src/components/NavDropdown.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-nav-item-dropdown 
+    class="ml-auto" 
+    :text="'Category: ' + activeCategory " 
+    type="dark" 
+    variant="dark"
+  >  
+    <b-dropdown-item 
+      v-for="category in categories"
+      :key="category"
+      :name="category.toLowerCase().split(' ').join('_')"
+      :active="activeCategory===category.toLowerCase().split(' ').join('_')"
+      @click="updateCategory"
+    >
+      {{category}}
+    </b-dropdown-item>
+  </b-nav-item-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'NavDropdown',
+  props: ['categories', 'activeCategory'],
+  methods: {
+    updateCategory: function(event) {
+      this.$emit('clicked', event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
