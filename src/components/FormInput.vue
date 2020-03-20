98c7bb5fb0e5526027af7bee3850db972a17fbe9
Travis Gee <geet084@gmail.com>
2020-03-20 15:42:51 -0600

    Adds formInput component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-form-group 
+    :label="name" 
+    :label-for="'dropdown-form-' + name.toLowerCase()" 
+    @submit.stop.prevent
+  >
+    <b-form-input
+      :id="'dropdown-form-' + name.toLowerCase()"
+      size="sm"
+      :placeholder="placeholder"
+    ></b-form-input>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  props: ['name', 'placeholder']
+}
+</script>
+
+<style scoped>
+
+</style>
