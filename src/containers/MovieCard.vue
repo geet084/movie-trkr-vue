0b1d227093c670546f129255ac768e3c5ea5759f
Travis Gee <geet084@gmail.com>
2020-03-23 20:45:00 -0600

    Adds movie card component

---
+++ src/containers/MovieCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-card
+    class="mb-4"
+    @click="handleMovieDetails"
+    :id="movie.id"
+    :img-src="'http://image.tmdb.org/t/p/w342'+ movie.poster_path"
+    :img-alt="movie.title"
+    overlay
+    tag="section"
+  >
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: ['movie'],
+  methods: {
+    handleMovieDetails({ target }) {
+      const movieId = target.closest('section').id
+      this.$emit('clicked', movieId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  cursor: pointer;
+  background-size: cover;
+  height: 28.45rem;
+  max-width: 19rem;
+  min-width: 19rem;
+  overflow: hidden;
+}
+.card:hover {
+  border: 2px solid #fff;
+}
+</style>
