3b1e62a85e0c1a4dfe1bfac3ce4066d7a8b77798
Travis Gee <geet084@gmail.com>
2020-03-23 20:55:22 -0600

    Adds main display component & movie detail handling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
   <div id="app">
-    <NavBar @clicked="updateCategory"/>
+    <NavBar @clicked="updateCategory" :userInfo="userInfo" />
+    <MainDisplay 
+      @clicked="getMovieDetails" 
+      :moviesData="moviesData" 
+      :movieDetails="movieDetails" 
+    />
   </div>
 </template>
 
 <script>
 import NavBar from './containers/NavBar.vue'
+import MainDisplay from './containers/MainDisplay.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    MainDisplay
   },
   data() {
     return {
-      movieData: [],
+      moviesData: [],
+      movieDetails: {},
+      userInfo: {
+        name: '',
+        email: ''
+      }
     }
   },
   mounted() {
     this.updateCategory('now_playing')
   },
   methods: {
-    updateCategory: function(category) {
+    updateCategory(category) {
       const apiKey = process.env.VUE_APP_MOVIE_DB_API_KEY
       const url = `https://api.themoviedb.org/3/movie/${category}?page=1&api_key=${apiKey}&language=en-US`
-
-      this.$http.get(url).then((res) => {
-        this.movieData = res.data.results
+      
+      this.$http.get(url).then(res => {
+        this.moviesData = res.data.results
+      })
+    },
+    getMovieDetails(movieId) {
+      const apiKey = process.env.VUE_APP_MOVIE_DB_API_KEY
+      const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`
+      
+      this.$http.get(url).then(res => {
+        this.movieDetails = res.data
       })
     }
   }
