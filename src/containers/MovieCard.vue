a68e4f9d643a5f62c301429d81456f5178903cb0
Travis Gee <geet084@gmail.com>
2020-03-25 15:30:20 -0600

    Adds star & toggle feature to movie card component

--- src/containers/MovieCard.vue
+++ src/containers/MovieCard.vue
@@ -8,17 +8,32 @@
     overlay
     tag="section"
   >
+    <span 
+      class="star"
+      :isStarred="isFave"
+      :class="{ starred: isStarred }"
+      @click.stop="toggleStarred"
+    >★</span>
   </b-card>
 </template>
 
 <script>
 export default {
   name: 'MovieCard',
-  props: ['movie'],
+  props: ['movie', 'isStarred'],
+  data() {
+    return {
+      isFave: false
+    }
+  },
   methods: {
     handleMovieDetails({ target }) {
       const movieId = target.closest('section').id
       this.$emit('clicked', movieId)
+    },
+    toggleStarred({ target }) {
+      const movieId = target.closest('section').id
+      this.$emit('toggleStarred', movieId)
     }
   }
 }
@@ -35,5 +50,19 @@ export default {
 }
 .card:hover {
   border: 2px solid #fff;
+}
+.star {
+  font-size: 2.5rem;
+  position: relative;
+  top: -15px;
+  left: 235px;
+  color: #fff;
+  text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+}
+.star:hover {
+  color:red;
+}
+.starred {
+  color: yellow;
 }
 </style>
