8e6d9c87e195718e5d16c20be7beb9d29910fc0b
Travis Gee <geet084@gmail.com>
2020-03-26 16:04:24 -0600

    Adds user options component for settings & signout

---
+++ src/components/UserOptions.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-nav-item-dropdown
+    :text="userInfo.name ? userInfo.name : 'Login'"
+    right
+  >
+    <b-dropdown-form class="signout">
+      <b-dropdown-item-button>
+        Settings
+      </b-dropdown-item-button>
+      <b-dropdown-item-button>
+        Sign Out {{userInfo.name}}
+      </b-dropdown-item-button>
+    </b-dropdown-form>
+  </b-nav-item-dropdown>
+</template>
+
+<script>
+export default {
+  name: 'UserOptions',
+  props: ['userInfo']
+}
+</script>
+
+<style scoped>
+
+</style>
