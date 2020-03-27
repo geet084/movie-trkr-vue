445a98c8065206f7658d7be586ba2634c08d095a
Travis Gee <geet084@gmail.com>
2020-03-27 16:28:46 -0600

    Adds basic user input handling & form submission

--- src/containers/LoginSignup.vue
+++ src/containers/LoginSignup.vue
@@ -6,12 +6,12 @@
     right
   >
     <b-dropdown-form class="login-menu">
-        <FormInput v-if="showSignUpForm" name="Name" placeholder="Jane Doe" />
-        <FormInput name="Email" placeholder="email@example.com" />
-        <FormInput name="Password" placeholder="Password" />
+        <FormInput v-if="showSignUpForm" name="Name" placeholder="Jane Doe" @inputChange="handleChange" :value="this.userData.name"/>
+        <FormInput name="Email" placeholder="email@example.com" @inputChange="handleChange" :value="this.userData.email"/>
+        <FormInput name="Password" placeholder="Password" @inputChange="handleChange" :value="this.userData.password"/>
 
         <b-form-checkbox class="mb-3" disabled>Remember me</b-form-checkbox>
-        <b-button variant="primary" size="sm" @click="toggleForm">
+        <b-button variant="primary" size="sm" @click="submitForm">
           {{showSignUpForm ? 'Sign Up' : ' Sign In'}}
         </b-button>
       </b-dropdown-form>
@@ -35,16 +35,38 @@ export default {
   },
   data() {
     return {
-      showSignUpForm: false
+      showSignUpForm: false,
+      userData: {
+        name: '',
+        email: '',
+        password: ''
+      }
     }
   },
   methods: {
-    toggleForm() {
-      this.$refs.loginDropdown.hide(true)
+    submitForm() {
+      let url = process.env.VUE_APP_DATABASE_URL
+      if (this.showSignUpForm) url += '/api/users/new'
+      else url += '/api/users/login'
+      
+      this.$http.post(url, this.userData).then(res => {
+        this.$emit('userData', res.data)
+        this.$refs.loginDropdown.hide(true)
+        this.userData = {
+          name: '',
+          email: '',
+          password: ''
+        }
+      }).catch(e => {
+        console.error('Login unsuccessful', e);
+      })
     },
     showSignUp() {
       this.showSignUpForm = !this.showSignUpForm
       this.$refs.loginDropdown.show(true)
+    },
+    handleChange(target) {
+      this.userData[target.name] = target.value
     }
   }
 }
