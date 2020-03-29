c07659c4c19143a22a47dff2baf4e5812411b131
Travis Gee <geet084@gmail.com>
2020-03-29 16:41:04 -0600

    Adds error field to form input

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -5,6 +5,7 @@
     @submit.stop.prevent
   >
     <b-form-input
+      :class="errorMsg ? 'showError' : ''"
       :id="'dropdown-form-' + name.toLowerCase()"
       @input="handleChange"
       :placeholder="placeholder"
@@ -12,22 +13,31 @@
       :type="name.toLowerCase() === 'password' ? 'password' : 'text'"
       :value="value"
     ></b-form-input>
+    <span v-if="errorMsg" class="error-message">{{errorMsg}}</span>
   </b-form-group>
 </template>
 
 <script>
 export default {
   name: 'FormInput',
-  props: ['name', 'placeholder', 'value'],
+  props: ['errorMsg', 'name', 'placeholder', 'value'],
   methods: {
     handleChange(value) {
       const target = { name: this.name.toLowerCase(), value }
       this.$emit('change', target)
+      this.$emit('resetError', this.name.toLowerCase())
     }
   }
 }
 </script>
 
 <style scoped>
+.showError {
+  border: 2px solid red;
+}
+.error-message {
+  color: red;
+  padding: 2px 0 0 2px;
+}
 
 </style>
