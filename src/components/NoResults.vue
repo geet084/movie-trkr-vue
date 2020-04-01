7eb4e29f76cb88fbdb8f029de2d4502b95713da5
Travis Gee <geet084@gmail.com>
2020-04-01 17:33:05 -0600

    Adds no results component

---
+++ src/components/NoResults.vue
@@ -0,0 +1,22 @@
+<template>
+  <article v-if="type === 'faves'" class="no-results">
+    <h2>Uh oh...</h2>
+    <h3>You don't have any favorites yet!</h3>
+    <p>Why not try adding some now?</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'NoResults',
+  props: ['type']
+}
+</script>
+
+<style scoped>
+.no-results {
+  color: #f8f9fa;
+  text-align: center;
+  padding-top: calc(15vw);
+}
+</style>
