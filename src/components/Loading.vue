80a62dc3c6ca798b73555fdb25e06b6aa3a686fc
Travis Gee <geet084@gmail.com>
2020-04-01 17:30:41 -0600

    Adds loading spinner component

---
+++ src/components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="loading">
+    <b-spinner
+      style="width: 3rem; height: 3rem;"
+      variant="light"
+    ></b-spinner>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style scoped>
+.loading {
+  color: #f8f9fa;
+  text-align: center;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: calc(50% - 25px);
+}
+</style>
