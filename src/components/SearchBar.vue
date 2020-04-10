6d690437d32f021df3635cee5c24b6d8b385ed41
Travis Gee <geet084@gmail.com>
2020-04-10 15:27:26 -0600

    Adds searchbar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-form-group
+    class="my-auto"
+    label-for="dropdown-form-searchbar" 
+    @submit.stop.prevent
+  >
+    <b-form-input
+      id="dropdown-form-searchbar"
+      @input="(value) => this.$emit('change', value)"
+      placeholder="Search"
+      size="sm"
+      type="text"
+      :value="value"
+    ></b-form-input>
+  </b-form-group>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: ['value']
+}
+</script>
+
+<style scoped>
+
+</style>
