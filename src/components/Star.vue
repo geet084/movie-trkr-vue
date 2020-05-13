2803b4ca312a454488a4d55ea8a254d808a975b7
Travis Gee <geet084@gmail.com>
2020-05-13 16:13:55 -0600

    Adds initial star component

---
+++ src/components/Star.vue
@@ -0,0 +1,18 @@
+<template>
+  <span
+    v-if="userInfo.name"
+    class="star"
+    :class="{ starred: isStarred }"
+    @click.stop="toggleStarred"
+  >★</span>
+</template>
+
+<script>
+export default {
+  name: 'Star'
+}
+</script>
+
+<style scoped>
+
+</style>
