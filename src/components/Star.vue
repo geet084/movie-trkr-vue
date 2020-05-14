b8c43f8da449d02b78350b97d4609d5de7f63972
Travis Gee <geet084@gmail.com>
2020-05-14 15:56:55 -0600

    Adds initial styling to star component

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -14,5 +14,19 @@ export default {
 </script>
 
 <style scoped>
-
+.star {
+  font-size: 2.5rem;
+  position: relative;
+  top: -15px;
+  left: 235px;
+  color: #fff;
+  text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+}
+.starred {
+  color: yellow;
+}
+.star:active, 
+.starred:active {
+  color:rgb(255, 255, 200);
+}
 </style>
