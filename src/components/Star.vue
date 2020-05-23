83deab51bb7313da70cfed15c9caa72444f7f3da
Travis Gee <geet084@gmail.com>
2020-05-23 15:56:10 -0600

    Refactors star styling to simplify

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -1,13 +1,13 @@
 <template>
   <span
     v-if="type === 'cardStar' && isLoggedIn"
-    class="cardStar"
+    class="star cardStar"
     :class="{ starred: isStarred }"
     @click.stop="toggleStarred"
   >★</span>
   <span
     v-else-if="type === 'detailsStar' && isLoggedIn"
-    class="detailsStar"
+    class="star detailsStar"
     :class="{ starred: isStarred }"
     @click.stop="toggleStarred"
   >★</span>
@@ -27,13 +27,15 @@ export default {
 </script>
 
 <style scoped>
+.star {
+  color: #fff;
+  text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+}
 .cardStar {
   font-size: 2.5rem;
   position: relative;
   top: -15px;
   left: 235px;
-  color: #fff;
-  text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
 }
 .detailsStar {
   font-size: 2rem;
