c084333e4aa2653698d0009514d62224e70dbab3
Travis Gee <geet084@gmail.com>
2020-05-27 14:15:18 -0600

    Adds movie genre component

---
+++ src/components/MovieGenres.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-card-text class="genres">
+    {{genreList}}
+  </b-card-text>
+</template>
+
+<script>
+export default {
+  name: 'MovieGenres',
+  props: ['movieGenres'],
+  data() {
+    return {
+      genreList: ''
+    }
+  },
+  mounted() {
+    this.buildGenreList(this.movieGenres)
+  },
+  methods: {
+    buildGenreList(genres) {
+      const lastWord = genres.length - 1
+
+      this.genreList = genres.map(genre => genre.name)
+        .sort()
+        .map((word, i) => i === lastWord ? `${word}` : `${word} ⋅ `)
+        .join('')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.genres {
+  color: #ccc;
+  font-family: 'Montserrat', sans-serif;
+  font-size: .8rem;
+}
+</style>
