dc66a733c7cf4f322c8117ef8fafd584f0e05d34
Travis Gee <geet084@gmail.com>
2020-05-31 15:20:11 -0600

    Installs vuex promise dependency

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vuex from 'vuex'
+import 'es6-promise/auto'
 
 Vue.config.productionTip = false
 
