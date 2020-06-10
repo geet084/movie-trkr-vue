1b89795b0ed201be0456db6d30df5d4beb782b7d
Travis Gee <geet084@gmail.com>
2020-06-09 21:34:25 -0600

    Adds store directory

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    userInfo: {
+      name: '',
+      email: ''
+    }
+  },
+  mutations: {
+    signOutUser: state => {
+      state.userInfo = {
+        name: '',
+        email: ''
+      }
+    }
+  }
+})
