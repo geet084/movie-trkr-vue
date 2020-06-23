845fb691d2335e8559ec18cec3a4092ee12ae042
Travis Gee <geet084@gmail.com>
2020-06-23 17:20:58 -0600

    Refactors to use separate url creation method

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,19 +2,23 @@ import axios from 'axios'
 
 export const actions = {
   getMovieData({ commit }, { type, selection }) {
-    const apiKey = process.env.VUE_APP_MOVIE_DB_API_KEY
-    const urlPrefix = `https://api.themoviedb.org/3`
-    const urlSuffix = `api_key=${apiKey}&language=en-US`
-    let url = ''
-    
-    if (type === 'movieDetails') url = urlPrefix + `/movie/${selection}?` + urlSuffix
-    else if (type === 'moviesList') url = urlPrefix + `/movie/${selection}?page=1&` + urlSuffix
-    else if (type === 'search') url = urlPrefix + `/search/movie?query=${selection}&` + urlSuffix
+    const url = createUrl(type, selection)
 
     axios.get(url).then(res => {
       if (type === 'movieDetails') commit('setMovieDetails', res.data)
       else if (type === 'moviesList') commit('setMoviesList', res.data.results)
-      else if (type === 'search') commit('moviesList', res.data.results)
+      else if (type === 'search') commit('setMoviesList', res.data.results)
     })
   }
+}
+
+
+const createUrl = (type, selection) => {
+  const apiKey = process.env.VUE_APP_MOVIE_DB_API_KEY
+  const urlPrefix = `https://api.themoviedb.org/3`
+  const urlSuffix = `api_key=${apiKey}&language=en-US`
+
+  if (type === 'movieDetails') return urlPrefix + `/movie/${selection}?` + urlSuffix
+  else if (type === 'moviesList') return urlPrefix + `/movie/${selection}?page=1&` + urlSuffix
+  else if (type === 'search') return urlPrefix + `/search/movie?query=${selection}&` + urlSuffix
 }
